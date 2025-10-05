b1a543928bc69f9ea2ed4725ab387fb2a099405a
aalbe32 <n11406119@qut.edu.au>
2025-10-05 19:46:00 +1000

    modified:   main.py 	new file:   templates/base.html 	new file:   templates/event_d_managment.html 	new file:   templates/index_dynamic.html

--- main.py
+++ main.py
@@ -5,7 +5,15 @@ app.debug = True
 
 @app.route('/')
 def landing():
-    return render_template('index.html')
+    return render_template('index_dynamic.html')
+
+@app.route('/managment')
+def eventManagment():
+    return render_template('event_d_managment.html')
+
+@app.route('/history')
+def eventHistory():
+    return render_template('booking-history.html')
 
 
 app.run()
