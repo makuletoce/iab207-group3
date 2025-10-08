ea2be47f12f7257fcdd36cb8e576c99487913aa5
Adam Albert <adam.albert@my.jcu.edu.au>
2025-10-08 12:27:52 +1000

    modified:   Tango/templates/base.html 	modified:   Tango/views.py

--- Tango/views.py
+++ Tango/views.py
@@ -18,17 +18,12 @@ def eventHistory():
 @mainbp.route('/details')
 def eventDetails():
     return render_template('event_details.html')
-<<<<<<< HEAD:main.py
 
-@app.route('/login')
+@mainbp.route('/login')
 def login():
     return render_template('Login.html')
 
-@app.route('/signup')
+@mainbp.route('/signup')
 def signup():
     return render_template('SignUp.html')
 
-
-app.run()
-=======
->>>>>>> 54350c9a5ba1453e5e23df25ad497378e8202b0e:Tango/views.py
