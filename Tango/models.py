897421f337e5d13ccedc5e1e38851d3e64627b57
aalbe32 <n11406119@qut.edu.au>
2025-10-15 14:09:37 +1000

    fixed missing collumn

--- Tango/models.py
+++ Tango/models.py
@@ -11,7 +11,7 @@ class Event(db.Model):
     status = db.Column(db.String(64), index=True, nullable=False)# Available, low-availability, sold out
     date = db.Column(db.Date, nullable=False)
     time = db.Column(db.Time, nullable=False)
-    catagory = image = db.Column(db.String(60), nullable=False, default="No Category") # casual, competative, social
+    catagory = db.Column(db.String(60), nullable=False, default="No Category") # casual, competative, social
     location = db.Column(db.String(500), nullable=False)
 
     host = db.Column(db.Integer, db.ForeignKey('users.id'))
